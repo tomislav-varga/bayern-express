c8824a44e7de4e495471be65fc3e11fc9f312c09
tomi <tomislav.varga@icloud.com>
2025-06-12 13:27:51 +0200

    change: address and phone number in contact section

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -164,8 +164,8 @@ const ContactSection: FC = () => {
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"></path>
                 </svg>
                 <address className="not-italic">
-                  Wasserstraße 123<br />
-                  80123 München
+                  Schatzbogen 33<br />
+                  81829 München
                 </address>
               </div>
               
@@ -173,7 +173,7 @@ const ContactSection: FC = () => {
                 <svg className="w-5 h-5 text-blue-300 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"></path>
                 </svg>
-                <span>089 12345678</span>
+                <span>+49 89 62099612</span>
               </div>
               
               <div className="flex items-center">
