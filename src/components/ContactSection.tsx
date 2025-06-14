c5f3b8ce837911001e86b3422d210f15c2bbb836
tomi <tomislav.varga@icloud.com>
2025-06-14 07:35:06 +0200

    add: address and telephone

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -319,7 +319,7 @@ const ContactSection: FC = () => {
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"></path>
                 </svg>
                 <div>
-                  <p><a href="tel:+498923539323" className="hover:underline">+49 (0) 89 235 39 323</a></p>
+                  <p><a href="tel:+498962099612" className="hover:underline">+49 (0) 89 620 996 12</a></p>
                 </div>
               </div>
               
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -65,15 +65,15 @@ const Footer: FC = () => {
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"></path>
                 </svg>
                 <span className="text-gray-400">
-                  Wasserstraße 123<br />
-                  80123 München
+                    Schatzbogen 33<br />
+                    81829 München
                 </span>
               </li>
               <li className="flex items-center">
                 <svg className="w-5 h-5 text-gray-400 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"></path>
                 </svg>
-                <span className="text-gray-400">089 12345678</span>
+                  <span><a href="tel:+498962099612" className="hover:underline">+49 (0) 89 620 996 12</a></span>
               </li>
               <li className="flex items-center">
                 <svg className="w-5 h-5 text-gray-400 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
