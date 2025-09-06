1f8b8f6b2acd77ae0775301bc76c31c463ad0f41
tomi <tomislav.varga@icloud.com>
2025-09-06 12:26:08 +0200

    change: dummy email

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -79,7 +79,7 @@ const Footer: FC = () => {
                 <svg className="w-5 h-5 text-gray-400 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path>
                 </svg>
-                <span className="text-gray-400">info@bayern-express.de</span>
+                <span className="text-gray-400">info@mail.test</span>
               </li>
             </ul>
           </div>
