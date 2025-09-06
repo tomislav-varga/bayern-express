eeab8ece272d68f046216d8245b44ab313fb8492
tomi <tomislav.varga@icloud.com>
2025-09-06 12:25:23 +0200

    change: dummy phone number

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -73,7 +73,7 @@ const Footer: FC = () => {
                 <svg className="w-5 h-5 text-gray-400 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"></path>
                 </svg>
-                  <span><a href="tel:+498962099612" className="hover:underline">+49 (0) 89 620 996 12</a></span>
+                  <span><a href="tel:+498962099612" className="hover:underline">+49 (0) 89 123 456 78</a></span>
               </li>
               <li className="flex items-center">
                 <svg className="w-5 h-5 text-gray-400 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
